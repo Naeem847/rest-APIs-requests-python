dcc24d88dd30735059c6a40b75038f95e7862bf4
Naeem847 <ashoornaeem@gmail.com>
2026-05-09 15:26:02 +0500

    A Python project demonstrating REST API integration using the requests module, including GET and POST requests, JSON handling, query parameters, and basic error handling.

--- Apis_request.py
+++ Apis_request.py
@@ -1,37 +1,59 @@
-# 1 making a get requests
+# # 1 making a get requests
+# import requests
+
+# url="https://jsonplaceholder.typicode.com/posts/1"
+
+# response= requests.get(url)
+
+# print(f"status code:{response.status_code}")
+# print("response content:", response.json())
+# # 2 making a post requests
+
+# url="https://jsonplaceholder.typicode.com/posts"
+
+# data={
+#     "title": "new post",
+#     "body": "this is the body of the new post",
+#     "userId":1
+# }
+
+# # send the post requests
+
+# response=requests.post(url,json=data)
+
+# print(f"status code:{response.status_code}")
+# print("response content:", response.json())
+
+# # Importance of Error Handling in APIs
+# url="https://jsonplaceholder.typicode.com/posts"
+
+# response=requests.get(url)
+
+# if response.status_code==200:
+#         print("request successful")
+#         # print("response content:", response.json())
+# else:
+#     print(f"error {response.status_code}: Unable to fetch data")
+#     print("request failed")
+
+# practise examples get requests with query parameters
+
+import  requests
+
+response=requests.get("https://jsonplaceholder.typicode.com/posts")
+if response.status_code==200:
+     
+     print("request successful")
+     print(response.status_code)
+     print(response.json())
+else:     
+     print("invalid requests")
+     print(response.status_code)
+#  example 2 of get request with query parameters
 import requests
 
-url="https://jsonplaceholder.typicode.com/posts/1"
+response=requests.get("https://jsonplaceholder.typicode.com/posts/1")
 
-response= requests.get(url)
+data=response.json()
 
-print(f"status code:{response.status_code}")
-print("response content:", response.json())
-# 2 making a post requests
-
-url="https://jsonplaceholder.typicode.com/posts"
-
-data={
-    "title": "new post",
-    "body": "this is the body of the new post",
-    "userId":1
-}
-
-# send the post requests
-
-response=requests.post(url,json=data)
-
-print(f"status code:{response.status_code}")
-print("response content:", response.json())
-
-# Importance of Error Handling in APIs
-url="https://jsonplaceholder.typicode.com/posts"
-
-response=requests.get(url)
-
-if response.status_code==200:
-        print("request successful")
-        # print("response content:", response.json())
-else:
-    print(f"error {response.status_code}: Unable to fetch data")
-    print("request failed")
+print(data)
